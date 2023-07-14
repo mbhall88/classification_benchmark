9e6579bd186210544aa957c4fb77b91a6b568871
Michael Hall <michael@mbh.sh>
2023-07-14 12:49:41 +1000

    add filter ambig

---
+++ workflow/scripts/filter_ambig.py
@@ -0,0 +1,24 @@
+import pysam
+import sys
+
+
+def ambig_prop(seq: str) -> float:
+    n_acgt = sum(1 for c in seq if c.upper() not in ("ACGT"))
+    return n_acgt / len(seq)
+
+
+def main():
+    if len(sys.argv) < 3:
+        raise ValueError("USAGE: filter_ambig.py <fastq> <max. ambig %>")
+    infile = sys.argv[1]
+    max_ambig = float(sys.argv[2])
+
+    with pysam.FastxFile(infile) as fh:
+        for read in fh:
+            if ambig_prop(read.sequence) > max_ambig:
+                continue
+            sys.stdout.write(str(read))
+
+
+if __name__ == "__main__":
+    main()
