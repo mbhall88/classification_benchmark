515fb5c67bd1ab35af3d8c2374b1bc9c63d4eca8
Michael Hall <michael@mbh.sh>
2023-08-09 06:20:44 +1000

    Update prepare_kraken_fasta.py

--- workflow/scripts/prepare_kraken_fasta.py
+++ workflow/scripts/prepare_kraken_fasta.py
@@ -39,11 +39,13 @@ def main():
         acc2file[acc] = p.resolve()
 
     acc2tax = dict()
-    asm_summary = Path(sys.argv[2])
+    asm_summary = Path(args.asm_summary)
     if not asm_summary.exists():
-        all_tax = sys.argv[2]
+        if not args.taxon:
+            raise ValueError("Need either --asm-summary or --taxon")
+        all_tax = args.taxon
     else:
-        with open(sys.argv[2]) as fp:
+        with open(asm_summary) as fp:
             for line in fp:
                 fields = line.split("\t")
                 acc = fields[0]
