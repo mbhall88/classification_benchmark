c5532a99fd1cff49d3341c7071f7822284259d76
Michael Hall <michael@mbh.sh>
2023-11-29 14:51:13 +1100

    make pod5 download a standalone script

--- workflow/scripts/download_and_subset_pod5.sh
+++ workflow/scripts/download_and_subset_pod5.sh
@@ -2,12 +2,18 @@
 
 set -euo pipefail
 
-exec 2>"${snakemake_log[0]}"
+# Parse command line options
+while getopts ":u:n:o:t:h" opt; do
+    case $opt in
+        u) URL="$OPTARG";;
+        n) N_READS="$OPTARG";;
+        o) OUTDIR="$OPTARG";;
+        t) THREADS="$OPTARG";;
+        h) echo "Usage: download_and_subset_pod5.sh -u <URL> -n <N_READS> -o <OUTDIR> -t <THREADS>"; exit 0;;
+        \?) echo "Invalid option -$OPTARG" >&2; exit 1;;
+    esac
+done
 
-URL="${snakemake_params[url]}"
-N_READS="${snakemake_params[n_reads]}"
-OUTDIR="${snakemake_output[pod5_dir]}"
-THREADS="${snakemake[threads]}"
 mkdir -p "$OUTDIR"
 
 tmpdir=$(mktemp -d)
@@ -31,10 +37,10 @@ target_mapping_file=read_ids.csv
 # the target_mapping_file needs to have the first column as the target filename
 # and the second column as the read id - https://github.com/nanoporetech/pod5-file-format/blob/master/python/pod5/README.md#target-mapping-csv
 csvtk -t cut -f read_id "$summary_file" |
-    sort --random-sort |
-    head -n "$N_READS" |
-    awk -v n=1 -v OFS=, '{print "batch" int((NR-1)/10)+n ".pod5", $0}' |
-    sed '1itarget,read_id' >"$target_mapping_file"
+        sort --random-sort |
+        head -n "$N_READS" |
+        awk -v n=1 -v OFS=, '{print "batch" int((NR-1)/10)+n ".pod5", $0}' |
+        sed '1itarget,read_id' >"$target_mapping_file"
 
 echo "Subsetting the pod5 files..." >&2
 
@@ -42,6 +48,9 @@ pod5 subset --csv "$target_mapping_file" --recursive -o "$OUTDIR" -t "$THREADS" 
 
 echo "Subset complete. Cleaning up..." >&2
 
-rm -rf "$tmpdir" "$target_mapping_file"
+rm -rf "$tmpdir"
+
+mv "$target_mapping_file" "$OUTDIR"
 
 echo "Done." >&2
+
