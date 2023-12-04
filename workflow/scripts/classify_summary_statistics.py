dfd8659a126437c3c61ecda84b8824db0d3a1cbc
Michael Hall <michael@mbh.sh>
2023-12-04 16:48:33 +1100

    add specificity and youdens index

--- workflow/scripts/classify_summary_statistics.py
+++ workflow/scripts/classify_summary_statistics.py
@@ -61,6 +61,35 @@ def f_score_interval(
     return f1_lwr_bound, f1_upr_bound
 
 
+def youden_index_interval(
+    tps: int, fps: int, fns: int, tns: int, conf: float = 0.95
+) -> tuple[float, float]:
+    """Calculate the Wilson score interval for the Youden index.
+    tps: Number of true positives
+    fps: Number of false positives
+    fns: Number of false negatives
+    tns: Number of true negatives
+    conf: the confidence level. i.e. 0.95 is 95% confidence
+    """
+    sn_lwr_bound, sn_upr_bound = confidence_interval(n_s=tps, n_f=fns)
+    specificity_lwr_bound, specificity_upr_bound = confidence_interval(n_s=tns, n_f=fps)
+
+    youden_lwr_bound = sn_lwr_bound + specificity_lwr_bound - 1
+    youden_upr_bound = sn_upr_bound + specificity_upr_bound - 1
+
+    return youden_lwr_bound, youden_upr_bound
+
+
+def youden_index(tps: int, fps: int, fns: int, tns: int) -> float:
+    """Calculate the Youden index.
+    tps: Number of true positives
+    fps: Number of false positives
+    fns: Number of false negatives
+    tns: Number of true negatives
+    """
+    return (tps / (tps + fns)) + (tns / (tns + fps)) - 1
+
+
 def summary(counts: Counter) -> tuple[int, int, int, int, float, float, float]:
     fns = counts["FN"]
     tps = counts["TP"]
@@ -80,12 +109,26 @@ def summary(counts: Counter) -> tuple[int, int, int, int, float, float, float]:
     f1_lwr_bound = round(f1_lwr_bound, SIGFIG)
     f1_upr_bound = round(f1_upr_bound, SIGFIG)
 
+    specificity = round(tns / (tns + fps), SIGFIG)
+    specificity_lwr_bound, specificity_upr_bound = confidence_interval(n_s=tns, n_f=fps)
+    specificity_lwr_bound = round(specificity_lwr_bound, SIGFIG)
+    specificity_upr_bound = round(specificity_upr_bound, SIGFIG)
+
+    youden = round(youden_index(tps=tps, fps=fps, fns=fns, tns=tns), SIGFIG)
+    youden_lwr_bound, youden_upr_bound = youden_index_interval(
+        tps=tps, fps=fps, fns=fns, tns=tns
+    )
+    youden_lwr_bound = round(youden_lwr_bound, SIGFIG)
+    youden_upr_bound = round(youden_upr_bound, SIGFIG)
+
     return (
         fns,
         fps,
         tns,
         tps,
+        (specificity, specificity_lwr_bound, specificity_upr_bound),
         (sn, sn_lwr_bound, sn_upr_bound),
+        (youden, youden_lwr_bound, youden_upr_bound),
         (precision, precision_lwr_bound, precision_upr_bound),
         (f1, f1_lwr_bound, f1_upr_bound),
     )
@@ -128,7 +171,9 @@ def main():
                     "FP",
                     "TN",
                     "TP",
-                    "Recall (95% CI)",
+                    "Specificity (95% CI)",
+                    "Sensitivity (95% CI)",
+                    "Youden's Index (95% CI)",
                     "Precision (95% CI)",
                     "F-score (95% CI)",
                 ]
@@ -171,7 +216,9 @@ def main():
                     "FP",
                     "TN",
                     "TP",
-                    "Recall (95% CI)",
+                    "Specificity (95% CI)",
+                    "Sensitivity (95% CI)",
+                    "Youden's Index (95% CI)",
                     "Precision (95% CI)",
                     "F-score (95% CI)",
                 ]
@@ -214,7 +261,9 @@ def main():
                     "FP",
                     "TN",
                     "TP",
-                    "Recall (95% CI)",
+                    "Specificity (95% CI)",
+                    "Sensitivity (95% CI)",
+                    "Youden's Index (95% CI)",
                     "Precision (95% CI)",
                     "F-score (95% CI)",
                 ]
@@ -257,7 +306,9 @@ def main():
                     "FP",
                     "TN",
                     "TP",
-                    "Recall (95% CI)",
+                    "Specificity (95% CI)",
+                    "Sensitivity (95% CI)",
+                    "Youden's Index (95% CI)",
                     "Precision (95% CI)",
                     "F-score (95% CI)",
                 ]
